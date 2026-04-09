2d3fc7c92d330e951d84591ad5003b0a53771d72
marcos <marcosdonarumo@gmail.com>
2026-04-09 14:50:49 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("Servidor da escola rodando")
+})
+
+app.get("/info", (req, res) => {
+    res.json({
+        escola: process.env.ESCOLA,
+        curso: process.env.CURSO,
+        professor: process.env.PROFESSOR,
+        ambiente: process.env.AMBIENTE
+    })
+})
+
+app.get("/alunos", (req, res) => {
+    res.json([
+        { nome: "João" },
+        { nome: "Maria" },
+        { nome: "Pedro" }
+    ])
+})
+
+app.listen(PORT, () => {
+    console.log(`rodando em http://localhost:${PORT}`)
+})
